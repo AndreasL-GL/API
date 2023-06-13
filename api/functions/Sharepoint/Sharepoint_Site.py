88e9f8670023c69c47fd6aecd2b62fd25ddd788e
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-06-13 15:24:03 +0200

    .

--- api/functions/Sharepoint/Sharepoint_Site.py
+++ api/functions/Sharepoint/Sharepoint_Site.py
@@ -1,6 +1,7 @@
 import json
 import os
-
+import requests
+from functions.Sharepoint.get_sharepoint_columns import get_sharepoint_access_headers_through_client_id
 def filter_sites(js):
     with open(os.path.join(os.path.dirname(__file__),'bad_sites.txt'), 'r', encoding=('utf-8')) as fp:
         bad_sites = fp.read().split('\n')
@@ -12,11 +13,31 @@ def filter_sites(js):
                 case 'OriginalPath': sitedict['Site'] = spobject['Value']
                 case 'Title': sitedict['Title'] = spobject['Value']
         if "personal" not in sitedict['Site'] and "(/portal)" not in sitedict['Site'] and sitedict['Title'] not in bad_sites:
-            sitelist.append(sitedict)
-        
+            try:
+                listlist = filter_lists(sitedict['Site'])
+                sitedict['Lists'] = listlist
+                sitelist.append(sitedict)
+            except:
+                pass
             
     return sitelist
 
+
+
+def filter_lists(js):
+    url = "https://greenlandscapingmalmo.sharepoint.com/sites/GLMalmAB-EgenkontrollerMalmstad"
+    js = requests.get(url + '/_api/web/lists', headers=get_sharepoint_access_headers_through_client_id()).json()
+    listlist = []
+    for item in js['d']['results']:
+        #print(item.keys())
+       # print(json.dumps(item, indent=4))
+        
+        if item["BaseTemplate"] == 100 and item['Title'] != "TaxonomyHiddenList":
+            listlist.append(item['Title'])
+    return listlist
+            
+
+        
 if __name__ == '__main__':
     with open(os.path.join(os.path.dirname(__file__),'spsite.json'),'r', encoding='utf-8') as f:
         js = json.load(f)
@@ -25,4 +46,4 @@ if __name__ == '__main__':
         json.dump(sitelist, f, indent=4)
     for item in sitelist:
         print(item['Title'])
-    
+    filter_lists(js)
