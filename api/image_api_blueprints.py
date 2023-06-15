fe8dbcf8b7f8f34f400e86a8ad539f4ada0e5d2a
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-06-15 15:58:05 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -8,21 +8,6 @@ image_resize = Blueprint('image_resize', __name__)
 
 @image_resize.route("/api/image_resizer", methods=["POST"])
 @require_api_key
-
-
-
-
-def upload():
-    # Get the file from the request
-    print("Hello")
-    file = request.files['file']
-    wb = openpyxl.load_workbook(file)
-    wb.save(os.path.join(os.path.dirname(__file__),'temp.xlsx'))
-    # Do whatever you need to do with the file here
-    # ...
-
-    return jsonify({"content": "Hello"})
-
 def post_file():
     file_content = request.json.get('content')
     width = int(request.json.get('width'))
@@ -31,5 +16,7 @@ def post_file():
     file_content=io.BytesIO(file_content)
     img_file=resize_and_autoorient(file_content, height,width)
     file_content=base64.b64encode(img_file.read()).decode('utf-8')
-    return jsonify({"content": file_content})
-    
+    return jsonify({
+        "$content-type": "image/jpeg",
+        "$content": file_content
+    })
