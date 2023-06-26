9c5f4322946827337bde97aa58058a7226a16bc5
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-06-26 16:49:33 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -24,11 +24,13 @@ def post_file():
 @image_resize.route("/api/image_resize", methods=["POST"])
 @require_api_key
 def post_file2():
-    file_content = request.json.get('File content')["$content"]
+    file_content = request.json.get('File Content')["$content"]
     width = int(request.json.get('width'))
+    height = int(request.json.get('height'))
+    if height==0:height=None
     file_content=base64.b64decode(file_content)
     file_content=io.BytesIO(file_content)
-    img_file=resize_and_autoorient(file_content,width,None)
+    img_file=resize_and_autoorient(file_content,width,height)
     file_content=base64.b64encode(img_file.read()).decode('utf-8')
     return jsonify({
         "$content-type": "image/jpeg",
