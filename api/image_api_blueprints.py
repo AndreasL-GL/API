bdbe427351a1a4478fb42d6f9d67df33596a9000
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-07-12 12:04:54 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -9,15 +9,16 @@ image_resize = Blueprint('image_resize', __name__)
 @image_resize.route("/api/image_resizer", methods=["POST"])
 @require_api_key
 def post_file():
-    file_content = request.json.get('$content')
+    file_content = request.json.get('File Content')["$content"]
     width = int(request.json.get('width'))
-    height = int(request.json.get('height'))
+    height = request.json.get('height')
+    if not height: height=None
     file_content=base64.b64decode(file_content)
     file_content=io.BytesIO(file_content)
-    img_file=resize_and_autoorient(file_content, height,width)
-    file_content=base64.b64encode(img_file.read()).decode('utf-8')
+    img_file=resize_and_autoorient(file_content,width=width,height=height)
+    file_content=base64.b64encode(img_file.getvalue).decode('utf-8')
     return jsonify({
-        "$content-type": "image/jpeg",
+        "$content-type": "image/png",
         "$content": file_content
     })
     
@@ -30,9 +31,21 @@ def post_file2():
     if not height: height=None
     file_content=base64.b64decode(file_content)
     file_content=io.BytesIO(file_content)
-    img_file=resize_and_autoorient(file_content.getvalue(),width=width,height=height)
-    file_content=base64.b64encode(img_file.read()).decode('utf-8')
+    img_file=resize_and_autoorient(file_content,width=width,height=height)
+    file_content=base64.b64encode(img_file.getvalue).decode('utf-8')
     return jsonify({
         "$content-type": "image/png",
         "$content": file_content
     })
+    
+if __name__=='__main__':
+    import json
+    with open(os.path.join(os.path.join(os.path.dirname(__file__),'functions'),'image.json'),'r') as f:
+        js = json.load(f)
+    bb = js['body']['File Content']
+    print(bb.keys())
+    img = resize_and_autoorient(io.BytesIO(base64.b64decode(bb["$content"])), width=200,height=None)
+    print(type(img))
+    from PIL import Image
+    im = Image.open(img)
+    im.show()
