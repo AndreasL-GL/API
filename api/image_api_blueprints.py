8c12c5176c4e4d50b9d0e2349e792743b5597e9b
Andreas Löfkvist <andreas.lofkvist@GLNET.SE>
2023-07-12 13:47:58 +0200

    .

--- api/image_api_blueprints.py
+++ api/image_api_blueprints.py
@@ -56,7 +56,7 @@ def resizer(js):
     with open(os.path.join(os.path.dirname(__file__),'image.json'), 'w') as f:
         json.dump(js,f,ensure_ascii=False)
     with open(os.path.join(os.path.dirname(__file__),'type.txt'), 'w') as f:
-        f.write(str(type(bb)))
+        f.write(str(type(bb))+str(bb[:200]) if type(bb)==str else type(bb))
     img = resize_and_autoorient(io.BytesIO(base64.b64decode(bb["$content"])), width=js['width'] if "width" in js.keys() else None,height=js['height'] if 'height' in js.keys() else None)
     return img
 if __name__=='__main__':
